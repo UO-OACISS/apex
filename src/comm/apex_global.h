13c6c6f3b70bd733ac9483636088eb364a304819
Kevin Huck <khuck@cs.uoregon.edu>
2015-02-10 14:13:14 -0800

    moving a header

---
+++ src/comm/apex_global.h
@@ -0,0 +1,29 @@
+#ifndef APEX_GLOBAL_H
+#define APEX_GLOBAL_H
+
+#include "apex.h"
+
+#ifdef __cplusplus
+extern "C"{
+#endif 
+
+// the function declaration, this is the funcion that does the reduction
+int action_apex_reduce(void *unused);
+
+// Each node has to populate their local value
+int action_apex_get_value(void *args);
+
+// A policy function to do periodic output
+int apex_periodic_policy_func(apex_context const context) ;
+
+// The function to set up global reductions
+void apex_global_setup(apex_function_address in_action);
+
+// The function to tear down global reductions, if necessary
+void apex_global_teardown(void);
+
+#ifdef __cplusplus
+}
+#endif 
+
+#endif
