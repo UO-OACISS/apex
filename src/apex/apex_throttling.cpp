2aaad0b11b39742d48e845e0da588631652574a0
Kevin Huck <khuck@cs.uoregon.edu>
2015-03-14 09:08:16 -0700

    Debugging throttling code. Cleaned up output messages.

--- src/apex/apex_throttling.cpp
+++ src/apex/apex_throttling.cpp
@@ -66,7 +66,7 @@ inline int __get_thread_cap(void) {
 }
 
 inline void __decrease_cap_gradual() {
-    thread_cap -= 2;
+    thread_cap -= 1;
     if (thread_cap < min_threads) { thread_cap = min_threads; }
     //printf("%d more throttling! new cap: %d\n", test_pp, thread_cap); fflush(stdout);
     apex_throttleOn = true;
@@ -83,7 +83,7 @@ inline void __decrease_cap() {
 }
 
 inline void __increase_cap_gradual() {
-    thread_cap += 2;
+    thread_cap += 1;
     if (thread_cap > max_threads) { thread_cap = max_threads; }
     //printf("%d less throttling! new cap: %d\n", test_pp, thread_cap); fflush(stdout);
     apex_throttleOn = false;
@@ -117,7 +117,7 @@ inline int apex_power_throttling_policy(apex_context const context)
       /* this is a hard limit! If we exceed the power cap once
          or in our moving average, then we need to adjust */
       --delay;
-      if (((power > max_watts) && 
+      if (((power > max_watts) ||
            (moving_average > max_watts)) && delay <= 0) { 
           __decrease_cap();
           delay = window_size;
@@ -491,7 +491,9 @@ inline void __read_common_variables() {
             if (envvar != NULL) {
                 min_threads = atoi(envvar);
             }
-            cout << "APEX Throttling enabled, min threads: " << min_threads << " max threads: " << max_threads << endl;
+            if (apex::apex::instance()->get_node_id() == 0) {
+                cout << "APEX Throttling enabled, min threads: " << min_threads << " max threads: " << max_threads << endl;
+            }
         }
     }
 }
@@ -512,7 +514,9 @@ inline int __setup_power_cap_throttling()
       if (getenv("APEX_ENERGY_THROTTLING") != NULL) {
         apex_energyThrottling = true;
       }
-      cout << "APEX Throttling for energy savings, min watts: " << min_watts << " max watts: " << max_watts << endl;
+      if (apex::apex::instance()->get_node_id() == 0) {
+        cout << "APEX Throttling for energy savings, min watts: " << min_watts << " max watts: " << max_watts << endl;
+      }
       apex::register_periodic_policy(1000000, apex_power_throttling_policy);
       // get an initial power reading
       apex::current_power_high();
