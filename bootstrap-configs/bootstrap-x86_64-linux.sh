57a5b2ef41f1de061019b09cfe73eeef14d5ad55
Kevin Huck <khuck@cs.uoregon.edu>
2015-04-30 10:58:13 -0700

    Change from TCmalloc to JEmalloc if available. If not, use TC.

--- bootstrap-configs/bootstrap-x86_64-linux.sh
+++ bootstrap-configs/bootstrap-x86_64-linux.sh
@@ -14,7 +14,7 @@ BOOST_ROOT=/usr
 # OPTIONAL libraries - if left undefined, they likely won't be used.
 
 BFD_ROOT=$HOME/install/binutils-2.23.2
-GPERFTOOLS_ROOT=$HOME/install/google-perftools/2.4
+#GPERFTOOLS_ROOT=$HOME/install/google-perftools/2.4
 #RCR_ROOT=$HOME/src/RCRdaemon
 #PAPI_ROOT=/usr/local/papi/5.3.2
 #TAU_ROOT=$HOME/install/tau-hpx
---
+++ cmake/Modules/FindJEmalloc.cmake
@@ -0,0 +1,54 @@
+# - Find JEmalloc
+# Find the native JEmalloc includes and library
+#
+#  JEmalloc_INCLUDE_DIR - where to find JEmalloc.h, etc.
+#  JEmalloc_LIBRARIES   - List of libraries when using JEmalloc.
+#  JEmalloc_FOUND       - True if JEmalloc found.
+
+
+if (JEmalloc_INCLUDE_DIR)
+  # Already in cache, be silent
+  set(JEmalloc_FIND_QUIETLY TRUE)
+endif ()
+
+find_path(JEmalloc_INCLUDE_DIR google/heap-checker.h
+  ${GPERFTOOLS_ROOT}/include
+  /opt/local/include
+  /usr/local/include
+  /usr/include
+)
+
+set(TMP_PATH $ENV{LD_LIBRARY_PATH})
+if ($TM_PATH)
+  string(REPLACE ":" " " LD_LIBRARY_PATH_STR $TMP_PATH)
+endif()
+set(JEmalloc_NAME jemalloc)
+find_library(JEmalloc_LIBRARY
+  NAME ${JEmalloc_NAME}
+  PATHS ${GPERFTOOLS_ROOT}/lib /usr/lib /usr/local/lib /opt/local/lib ${LD_LIBRARY_PATH_STR}
+)
+
+if (JEmalloc_INCLUDE_DIR AND JEmalloc_LIBRARY)
+   set(JEmalloc_FOUND TRUE)
+    set( JEmalloc_LIBRARIES ${JEmalloc_LIBRARY} )
+else ()
+   set(JEmalloc_FOUND FALSE)
+   set( JEmalloc_LIBRARIES )
+endif ()
+
+if (JEmalloc_FOUND)
+   if (NOT JEmalloc_FIND_QUIETLY)
+      message(STATUS "Found JEmalloc: ${JEmalloc_LIBRARY}")
+   endif ()
+else ()
+      message(STATUS "Not Found JEmalloc: ${JEmalloc_LIBRARY}")
+   if (JEmalloc_FIND_REQUIRED)
+      message(STATUS "Looked for JEmalloc libraries named ${JEmallocS_NAME}.")
+      message(FATAL_ERROR "Could NOT find JEmalloc library")
+   endif ()
+endif ()
+
+mark_as_advanced(
+  JEmalloc_LIBRARY
+  JEmalloc_INCLUDE_DIR
+)
