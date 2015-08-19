dfcf3b31addc89013c0558899b9fc0094b05f4b4
Kevin Huck <khuck@cs.uoregon.edu>
2015-08-19 08:22:58 -0700

    Updating TAU support

--- bootstrap-configs/bootstrap-x86_64-linux.sh
+++ bootstrap-configs/bootstrap-x86_64-linux.sh
@@ -13,14 +13,15 @@ BOOST_ROOT=/usr
 
 # OPTIONAL libraries - if left undefined, they likely won't be used.
 
-export BFD_ROOT=/usr
+#export BFD_ROOT=/usr
+export BFD_ROOT=/src/tau2/x86_64/binutils-2.23.2
 #export JEMALLOC_ROOT=/home3/khuck/install/jemalloc/3.6.0
 #export GPERFTOOLS_ROOT=$HOME/install/google-perftools/2.4
 #export RCR_ROOT=$HOME/src/RCRdaemon
 #export PAPI_ROOT=/usr/local/papi/5.3.2
-#export TAU_ROOT=$HOME/install/tau-hpx
+export TAU_ROOT=$HOME/src/tau2
 #export OMPT_ROOT=$HOME/install/libiomp5
-export ACTIVEHARMONY_ROOT=$HOME/install/activeharmony/4.5
+#export ACTIVEHARMONY_ROOT=$HOME/install/activeharmony/4.5
 
 # other CMake variables - for special situations / architectures / compilers.
 
@@ -32,13 +33,6 @@ cmake_use_codeblocks="-G \"CodeBlocks - Unix Makefiles\"" # if you want to debug
 cmake_make_verbose=""  # for verbose, use -DCMAKE_VERBOSE_MAKEFILE=ON
 cmake_use_mpi="-DUSE_MPI=FALSE" # TRUE or FALSE
 cmake_other_settings="-DUSE_LM_SENSORS=FALSE" # anything else?
-
-# runtime parameters for testing APEX with "make test"
-
-export APEX_POLICY=1
-export APEX_THROTTLING=1
-export APEX_CONCURRENCY=0
-export APEX_TAU=0
 
 ###################################################################
 # NO NEED TO MODIFY ANYTHING BELOW THIS LINE
