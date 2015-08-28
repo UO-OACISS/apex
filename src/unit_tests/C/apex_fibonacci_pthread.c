dc6014882538b5ae15bb83d9e63d4d8883e8ba7e
Kevin Huck <khuck@cs.uoregon.edu>
2015-08-28 12:27:35 -0700

    Bug in the stack size definition.

--- src/unit_tests/C/apex_fibonacci_pthread.c
+++ src/unit_tests/C/apex_fibonacci_pthread.c
@@ -3,7 +3,9 @@
 #include <stdlib.h>
 #include "apex.h"
 
-#define THREADSTACK  1024*32
+#ifndef PTHREAD_STACK_MIN
+#define PTHREAD_STACK_MIN 1024*16
+#endif
 
 #define FIB_RESULTS_PRE 41
 long long fib_results[FIB_RESULTS_PRE] = {0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597,2584,4181,6765,10946,17711,28657,46368,75025,121393,196418,317811,514229,832040,1346269,2178309,3524578,5702887,9227465,14930352,24157817,39088169,63245986,102334155};
@@ -24,7 +26,7 @@ void * fib (void * in) {
     long long result_a = 0;
 	pthread_attr_t attr_a; 
 	pthread_attr_init(&attr_a);
-	pthread_attr_setstacksize(&attr_a, THREADSTACK);
+	pthread_attr_setstacksize(&attr_a, PTHREAD_STACK_MIN);
 	pthread_t thread_a;
 	pthread_create(&thread_a,&attr_a,fib,(void*)a);
     pthread_attr_destroy(&attr_a);
