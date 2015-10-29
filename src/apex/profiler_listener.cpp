1c12467892258ec20994c6b8bc7663f3652aa974
Kevin Huck <khuck@cs.uoregon.edu>
2015-10-29 08:29:21 -0700

    The "emplace" function isn't defined for Intel Mic compiler.

--- src/apex/profiler_listener.cpp
+++ src/apex/profiler_listener.cpp
@@ -301,15 +301,15 @@ namespace apex {
       // if this is a new dependency for this parent?
       if (it == task_dependencies.end()) {
           depend = new unordered_map<task_identifier, int>();
-          depend->emplace(*td->child, 1);
-          task_dependencies.emplace(*td->parent, depend);
+          (*depend)[*td->child] = 1;
+          task_dependencies[*td->parent] = depend;
       // otherwise, see if this parent has seen this child
       } else {
           depend = it->second;
           unordered_map<task_identifier, int>::const_iterator it2 = depend->find(*td->child);
           // first time for this child
           if (it2 == depend->end()) {
-              depend->emplace(*td->child, 1);
+              (*depend)[*td->child] = 1;
           // not the first time for this child
           } else {
               int tmp = it2->second;
