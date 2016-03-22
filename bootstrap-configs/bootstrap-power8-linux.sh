e028ef6b0f21d27623b233126062df11c4e84c7a
Kevin Huck <khuck@cs.uoregon.edu>
2016-03-22 15:57:25 -0700

    cleaning up tests and examples so they don't build by default, and they aren't installed.

--- bootstrap-configs/bootstrap-power8-linux.sh
+++ bootstrap-configs/bootstrap-power8-linux.sh
@@ -3,7 +3,7 @@
 # where is this script located?
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-# WORKDIR=/tmp
+WORKDIR=/tmp
 
 ###################################################################
 #configure parameters - set what ever you need in this top section!
@@ -11,7 +11,7 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 # REQUIRED libraries
 
-export BOOST_ROOT=/usr/local/packages/boost/1.58.0/power8_gcc-4.9.2_release
+# None!
 
 # OPTIONAL libraries - if left undefined, they likely won't be used.
 
@@ -27,14 +27,14 @@ export ACTIVEHARMONY_ROOT=$HOME/install/activeharmony/4.5
 
 # other CMake variables - for special situations / architectures / compilers.
 
-cmake_build_type="-DCMAKE_BUILD_TYPE=Debug" # Debug, Release, RelWithDebInfo, etc.
+cmake_build_type="-DCMAKE_BUILD_TYPE=RelWithDebInfo" # Debug, Release, RelWithDebInfo, etc.
 cmake_apex_throttle="-DAPEX_THROTTLE=TRUE" # TRUE or FALSE
 cmake_build_shared_libs="-DBUILD_SHARED_LIBS=TRUE" # TRUE or FALSE
 cmake_install_prefix="-DCMAKE_INSTALL_PREFIX=${DIR}/../install/power8_gcc-4.9.2" # the installation path
 cmake_use_codeblocks="-G \"CodeBlocks - Unix Makefiles\"" # if you want to debug in CodeBlocks
 cmake_make_verbose=""  # for verbose, use -DCMAKE_VERBOSE_MAKEFILE=ON
 cmake_use_mpi="-DUSE_MPI=FALSE" # TRUE or FALSE 
-cmake_other_settings="-DBUILD_EXAMPLES=TRUE" # anything else?
+cmake_other_settings="" # anything else?
 
 ###################################################################
 # NO NEED TO MODIFY ANYTHING BELOW THIS LINE
