bca4ab9d294c02c9df7cdb02e88fa0da5b140c30
Kevin Huck <khuck@cs.uoregon.edu>
2016-04-19 10:00:04 -0700

    Adding support for SOSflow!

---
+++ src/apex/sos_handler.hpp
@@ -0,0 +1,29 @@
+//  Copyright (c) 2014 University of Oregon
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+
+#include "handler.hpp"
+#include <memory>
+#include "sos.h"
+
+namespace apex {
+
+class sos_handler : public handler {
+private:
+  bool _terminate;
+  SOS_runtime * _runtime;
+  SOS_pub * _pub;
+  void _make_pub (void);
+  SOS_val timer;
+  SOS_val counter;
+public:
+  sos_handler (int argc, char * argv[], int period);
+  ~sos_handler (void);
+  virtual bool _handler(void);
+};
+
+}
+
