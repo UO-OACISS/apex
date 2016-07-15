94c73451f21698c9eecd8359b0ccd35ebf79804e
Kevin Huck <khuck@cs.uoregon.edu>
2016-07-15 14:08:40 -0700

    Working OTF2. Whew.

--- src/apex/otf2_listener.cpp
+++ src/apex/otf2_listener.cpp
@@ -30,6 +30,9 @@ namespace apex {
 
 
     void otf2_listener::on_startup(startup_event_data &data) {
+        /* get a start time for the trace */
+        using namespace std::chrono;
+        this->globalOffset = duration_cast<microseconds>(system_clock::now().time_since_epoch()).count();
         /* open the OTF2 archive */
         archive = OTF2_Archive_Open( apex_options::otf2_archive_path(),
                 apex_options::otf2_archive_name(),
@@ -57,7 +60,11 @@ namespace apex {
     }
 
     void otf2_listener::write_otf2_regions(void) {
-        auto region_indices = get_region_indices();
+        // only write these out once!
+        static __thread bool written = false;
+        if (written) return;
+        written = true;
+        auto region_indices = get_global_region_indices();
         for (auto const &i : region_indices) {
             task_identifier id = i.first;
             uint64_t idx = i.second;
@@ -78,15 +85,24 @@ namespace apex {
 
     void otf2_listener::on_shutdown(shutdown_event_data &data) {
         APEX_UNUSED(data);
+        write_otf2_regions();
         if (!_terminate) {
             _terminate = true;
             /* close event files */
             OTF2_Archive_CloseEvtFiles( archive );
             /* write the clock properties */
+            /*
+            uint64_t ticks_per_second = (uint64_t)(1.0/profiler::get_cpu_mhz());
+            uint64_t globalOffset = profiler::time_point_to_nanoseconds(profiler::get_global_start());
+            uint64_t traceLength = profiler::time_point_to_nanoseconds(profiler::get_global_end());
+            */
+            uint64_t ticks_per_second = 1000000;
+            using namespace std::chrono;
+            uint64_t traceLength = duration_cast<microseconds>(system_clock::now().time_since_epoch()).count();
             OTF2_GlobalDefWriter_WriteClockProperties( global_def_writer,
-                    1 /* 1 tick per second */,
-                    0 /* epoch */,
-                    2 /* length */ );
+                    ticks_per_second /* 1,000,000,000 ticks per second */,
+                    this->globalOffset /* epoch */,
+                    traceLength /* length */ );
             /* define some strings */
             stringstream locality;
             locality << "process " << apex::__instance()->get_node_id();
@@ -98,15 +114,27 @@ namespace apex {
             string node("node");
             OTF2_GlobalDefWriter_WriteString( global_def_writer, get_string_index(node), node.c_str() );
             OTF2_GlobalDefWriter_WriteSystemTreeNode( global_def_writer,
-                    get_string_index(empty),
-                    get_string_index(host),
-                    get_string_index(node),
-                    OTF2_UNDEFINED_SYSTEM_TREE_NODE /* parent */ );
+                0, /* System Tree Node ID */
+                get_string_index(host), /* host name string ID */
+                get_string_index(node), /* class name string ID */
+                OTF2_UNDEFINED_SYSTEM_TREE_NODE /* parent */ );
             OTF2_GlobalDefWriter_WriteLocationGroup( global_def_writer,
-                    get_string_index(empty) /* id */,
-                    get_string_index(locality.str()) /* name */,
-                    OTF2_LOCATION_GROUP_TYPE_PROCESS,
-                    get_string_index(empty) /* system tree */ );
+                apex::__instance()->get_node_id() /* id */,
+                get_string_index(locality.str()) /* name */,
+                OTF2_LOCATION_GROUP_TYPE_PROCESS,
+                0 /* system tree node ID */ );
+            /* write out the thread locations */
+            for (int i = 0 ; i < thread_instance::get_num_threads() ; i++) {
+                stringstream thread;
+                thread << "thread " << i;
+                OTF2_GlobalDefWriter_WriteString( global_def_writer, get_string_index(thread.str()), thread.str().c_str() );
+                OTF2_GlobalDefWriter_WriteLocation( global_def_writer, 
+                    i /* id */,
+                    get_string_index(thread.str()) /* name */,
+                    OTF2_LOCATION_TYPE_CPU_THREAD,
+                    get_global_region_indices().size() /* number of events */,
+                    apex::__instance()->get_node_id() /* location group ID */ );
+            }
             OTF2_Archive_Close( archive );
         }
         return;
@@ -120,24 +148,19 @@ namespace apex {
 
     void otf2_listener::on_new_thread(new_thread_event_data &data) {
         if (!_terminate) {
+            /* get an event writer for this thread */
             evt_writer = OTF2_Archive_GetEvtWriter( archive, thread_instance::get_id() );
+            /* get the definition writer so we can record strings */
+            def_writer = OTF2_Archive_GetDefWriter( archive, thread_instance::get_id() );
         }
         return;
     }
 
     void otf2_listener::on_exit_thread(event_data &data) {
         if (!_terminate) {
+            //write_otf2_regions();
             OTF2_Archive_CloseDefWriter( archive, def_writer );
             OTF2_Archive_CloseEvtWriter( archive, evt_writer );
-            write_otf2_regions();
-            stringstream thread;
-            thread << "thread " << thread_instance::get_id();
-            OTF2_GlobalDefWriter_WriteString( global_def_writer, get_string_index(thread.str()), thread.str().c_str() );
-            OTF2_GlobalDefWriter_WriteLocation( global_def_writer, thread_instance::get_id() /* id */,
-                        get_string_index(thread.str()) /* name */,
-                        OTF2_LOCATION_TYPE_CPU_THREAD,
-                        get_region_indices().size() /* # events */,
-                        get_string_index(empty) /* location group */ );
         }
         APEX_UNUSED(data);
         return;
@@ -145,7 +168,15 @@ namespace apex {
 
     bool otf2_listener::on_start(task_identifier * id) {
         if (!_terminate) {
-            OTF2_EvtWriter_Enter( evt_writer, NULL, get_time(), get_region_index(id) /* region */ );
+          /*
+            profiler * p = thread_instance::instance().get_current_profiler();
+            uint64_t stamp = profiler::time_point_to_nanoseconds(p->start); 
+                     */
+            using namespace std::chrono;
+            uint64_t stamp = duration_cast<microseconds>(system_clock::now().time_since_epoch()).count();
+            OTF2_EvtWriter_Enter( evt_writer, NULL, 
+                stamp,
+                get_region_index(id) /* region */ );
         } else {
             return false;
         }
@@ -158,7 +189,14 @@ namespace apex {
 
     void otf2_listener::on_stop(std::shared_ptr<profiler> &p) {
         if (!_terminate) {
-            OTF2_EvtWriter_Leave( evt_writer, NULL, get_time(), get_region_index(p->task_id) /* region */ );
+          /*
+            uint64_t stamp = profiler::time_point_to_nanoseconds(p->end);
+                     */
+            using namespace std::chrono;
+            uint64_t stamp = duration_cast<microseconds>(system_clock::now().time_since_epoch()).count();
+            OTF2_EvtWriter_Leave( evt_writer, NULL, 
+                stamp,
+                get_region_index(p->task_id) /* region */ );
         }
         return;
     }
