cdcdb917ff27d59fbe4044922dcc7327f24e9893
Kevin Huck <khuck@cs.uoregon.edu>
2016-09-28 15:29:28 -0700

    Adding "download and build" support for OMPT and OTF2.

--- cmake/Modules/FindOMPT.cmake
+++ cmake/Modules/FindOMPT.cmake
@@ -20,7 +20,7 @@ set(OMPT_DEFINITIONS ${PC_OMPT_CFLAGS_OTHER})
 find_path(OMPT_INCLUDE_DIR ompt.h
           HINTS ${PC_OMPT_INCLUDEDIR} ${PC_OMPT_INCLUDE_DIRS} ${OMPT_ROOT}/include)
 
-find_library(OMPT_LIBRARY NAMES gomp iomp5
+find_library(OMPT_LIBRARY NAMES omp iomp5 gomp 
              HINTS ${PC_OMPT_LIBDIR} ${PC_OMPT_LIBRARY_DIRS} ${OMPT_ROOT}/lib 
 			 ${OMPT_ROOT}/lib/* NO_DEFAULT_PATH)
 
@@ -31,6 +31,38 @@ find_package_handle_standard_args(OMPT  DEFAULT_MSG
                                   OMPT_LIBRARY OMPT_INCLUDE_DIR)
 
 mark_as_advanced(OMPT_INCLUDE_DIR OMPT_LIBRARY)
+
+# --------- DOWNLOAD AND BUILD THE EXTERNAL PROJECT! ------------ #
+if(BUILD_OMPT OR (NOT OMPT_FOUND))
+  set(CACHE OMPT_ROOT ${CMAKE_INSTALL_PREFIX} STRING "OMPT Root directory")
+  message("Attention: Downloading and Building OMPT as external project!")
+  message(INFO " A working internet connection is required!")
+  include(ExternalProject)
+  ExternalProject_Add(project_ompt
+    GIT_REPOSITORY https://github.com/khuck/LLVM-openmp.git
+	GIT_TAG v0.1
+    PREFIX ${CMAKE_CURRENT_BINARY_DIR}/LLVM-ompt-0.1
+    CONFIGURE_COMMAND cmake -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER} -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER} -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} -DCMAKE_BUILD_TYPE=Release ../project_ompt
+    BUILD_COMMAND make
+    INSTALL_COMMAND make install
+    INSTALL_DIR ${CMAKE_INSTALL_PREFIX}
+    LOG_DOWNLOAD 1
+  )
+  set(OMPT_ROOT ${CMAKE_INSTALL_PREFIX})
+  #ExternalProject_Get_Property(project_ompt install_dir)
+  add_library(omp SHARED IMPORTED)
+  set_property(TARGET omp PROPERTY IMPORTED_LOCATION ${CMAKE_INSTALL_PREFIX}/lib/libomp.so)
+  set(OMPT_INCLUDE_DIR "${OMPT_ROOT}/include")
+  set(OMPT_LIBRARY "${OMPT_ROOT}/lib/libomp.so")
+  # handle the QUIETLY and REQUIRED arguments and set OMPT_FOUND to TRUE
+  # if all listed variables are TRUE
+  find_package_handle_standard_args(OMPT  DEFAULT_MSG
+                                    OMPT_LIBRARY OMPT_INCLUDE_DIR)
+  set(OMPT_FOUND TRUE)
+else()
+  add_custom_target(project_ompt)
+endif()
+# --------- DOWNLOAD AND BUILD THE EXTERNAL PROJECT! ------------ #
 
 if(OMPT_FOUND)
   set(OMPT_LIBRARIES ${OMPT_LIBRARY} )
--- cmake/Modules/FindOTF2.cmake
+++ cmake/Modules/FindOTF2.cmake
@@ -31,7 +31,38 @@ find_package_handle_standard_args(OTF2  DEFAULT_MSG
 
 mark_as_advanced(OTF2_INCLUDE_DIR OTF2_LIBRARY)
 
-add_custom_target(project_otf2)
+# --------- DOWNLOAD AND BUILD THE EXTERNAL PROJECT! ------------ #
+if(BUILD_OTF2 OR (NOT OTF2_FOUND))
+  set(CACHE OTF2_ROOT ${CMAKE_INSTALL_PREFIX} STRING "OTF2 Root directory")
+  message("Attention: Downloading and Building OTF2 as external project!")
+  message(INFO " A working internet connection is required!")
+  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fPIC")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fPIC")
+  include(ExternalProject)
+  ExternalProject_Add(project_otf2
+    URL http://www.vi-hps.org/upload/packages/otf2/otf2-2.0.tar.gz
+    PREFIX ${CMAKE_CURRENT_BINARY_DIR}/otf2-2.0
+    CONFIGURE_COMMAND cd ${CMAKE_CURRENT_BINARY_DIR}/otf2-2.0/src/project_otf2 && ./configure CC=${CMAKE_C_COMPILER} CXX=${CMAKE_CXX_COMPILER} CFLAGS=${CMAKE_C_FLAGS} CXXFLAGS=${CMAKE_CXX_FLAGS} LDFLAGS=${CMAKE_EXE_LINKER_FLAGS} --prefix=${CMAKE_INSTALL_PREFIX} --enable-shared
+    BUILD_COMMAND cd ${CMAKE_CURRENT_BINARY_DIR}/otf2-2.0/src/project_otf2 && make
+    INSTALL_COMMAND cd ${CMAKE_CURRENT_BINARY_DIR}/otf2-2.0/src/project_otf2 && make install
+    INSTALL_DIR ${CMAKE_INSTALL_PREFIX}
+    LOG_DOWNLOAD 1
+  )
+  set(OTF2_ROOT ${CMAKE_INSTALL_PREFIX})
+  #ExternalProject_Get_Property(project_otf2 install_dir)
+  add_library(otf2 STATIC IMPORTED)
+  set_property(TARGET otf2 PROPERTY IMPORTED_LOCATION ${CMAKE_INSTALL_PREFIX}/lib/libotf2.a)
+  set(OTF2_INCLUDE_DIR "${OTF2_ROOT}/include")
+  set(OTF2_LIBRARY "${OTF2_ROOT}/lib/libotf2.a")
+  # handle the QUIETLY and REQUIRED arguments and set OTF2_FOUND to TRUE
+  # if all listed variables are TRUE
+  find_package_handle_standard_args(OTF2  DEFAULT_MSG
+                                    OTF2_LIBRARY OTF2_INCLUDE_DIR)
+  set(OTF2_FOUND TRUE)
+else()
+  add_custom_target(project_otf2)
+endif()
+# --------- DOWNLOAD AND BUILD THE EXTERNAL PROJECT! ------------ #
 
 if(OTF2_FOUND)
   set(OTF2_LIBRARIES ${OTF2_LIBRARY} )
