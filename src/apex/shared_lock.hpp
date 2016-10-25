53408ac8234e02a4cbd94106b6834f85446c65ea
Kevin Huck <khuck@cs.uoregon.edu>
2016-10-24 17:14:51 -0700

    Forgot to add a header.

---
+++ src/apex/shared_lock.hpp
@@ -0,0 +1,34 @@
+//  Copyright (c) 2014 University of Oregon
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+
+#if __cplusplus > 201701L 
+#include <shared_mutex>
+#elif __cplusplus > 201402L
+#include <shared_lock>
+#else
+#include <mutex>
+#endif
+
+namespace apex
+{
+
+#if __cplusplus > 201701L 
+    typedef std::shared_mutex shared_mutex_type;
+    typedef std::shared_lock<shared_mutex_type> read_lock_type;
+    typedef std::unique_lock<shared_mutex_type> write_lock_type;
+#elif __cplusplus > 201402L
+    typedef std::shared_lock shared_mutex_type;
+    typedef std::shared_lock<shared_mutex_type> read_lock_type;
+    typedef std::unique_lock<shared_mutex_type> write_lock_type;
+#else
+    typedef std::mutex shared_mutex_type;
+    typedef std::unique_lock<shared_mutex_type> read_lock_type;
+    typedef std::unique_lock<shared_mutex_type> write_lock_type;
+#endif
+
+}
+
