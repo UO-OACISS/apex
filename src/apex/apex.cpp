bfc995497499e6169e469e117f0f44bd9d197a3c
Kevin Huck <khuck@cs.uoregon.edu>
2017-10-23 11:55:11 -0700

    Removing debug message

--- src/apex/apex.cpp
+++ src/apex/apex.cpp
@@ -347,12 +347,10 @@ uint64_t init(const char * thread_name, uint64_t comm_rank, uint64_t comm_size) 
     if (_registered || _initialized) { 
         /* check to see if APEX was initialized by OMPT before MPI had a chance
          * to pass in any values */
-        printf("UPDATING RANK AND SIZE?\n");
         if ((comm_rank < comm_size) && (comm_size > 1)) { // simple validation
             apex* instance = apex::instance(); // get/create the Apex static instance
             instance->set_node_id(comm_rank);
             instance->set_num_ranks(comm_size);
-            printf("UPDATING RANK AND SIZE to %llu and %llu\n", comm_rank, comm_size); fflush(stdout);
             for (unsigned int i = 0 ; i < instance->listeners.size() ; i++) {
                 instance->listeners[i]->set_node_id((int)comm_rank, (int)comm_size);
             }
