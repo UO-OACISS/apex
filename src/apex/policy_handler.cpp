efca32f9e711fbf5cc4a3bb90aada42deab5457b
Kevin Huck <khuck@cs.uoregon.edu>
2018-09-03 17:10:36 -0700

    Split APEX_HAVE_HPX into another macro, APEX_HAVE_HPX_CONFIG
    This is annoying and confusing.  We have to set a define so that the
    HPX config file will be included, which will define APEX_HAVE_HPX
    for us.  We can't use the same name because then the macro is
    defined twice.  So, we have a macro to make sure the macro is defined.
    This is all because of circular dependencies between HPX and APEX.

--- src/apex/policy_handler.cpp
+++ src/apex/policy_handler.cpp
@@ -3,7 +3,11 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#ifdef APEX_HAVE_HPX
+/* This is annoying and confusing.  We have to set a define so that the
+ * HPX config file will be included, which will define APEX_HAVE_HPX
+ * for us.  We can't use the same name because then the macro is defined
+ * twice.  So, we have a macro to make sure the macro is defined. */
+#ifdef APEX_HAVE_HPX_CONFIG
 #include <hpx/config.hpp>
 #include <hpx/include/runtime.hpp>
 #include <boost/bind.hpp>
