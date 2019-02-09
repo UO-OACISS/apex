3605033f3059e71805add94eb789aefb6609e62b
Kevin Huck <khuck@cs.uoregon.edu>
2019-02-08 16:51:45 -0800

    Cleaning up release script

--- etc/maketag.sh
+++ etc/maketag.sh
@@ -1,11 +1,12 @@
 
-oldtag=v2.0
-tagname=v2.0.1
+oldtag=v2.1.1
+tagname=v2.1.2
 
 echo "Commit Log:" >> ${tagname}.txt
 
-git log ${oldtag}..HEAD --pretty=format:'http://github.com/khuck/xpress-apex/commit/%H %ad %s ' --reverse >> ${tagname}.txt
+git log ${oldtag}..HEAD --pretty=format:'<li> <a href="http://github.com/khuck/xpress-apex/commit/%H">view commit &bull;</a> %s</li> ' --reverse > ${tagname}.txt
+# git log ${oldtag}..HEAD --pretty=format:'http://github.com/khuck/xpress-apex/commit/%H %ad %s ' --reverse >> ${tagname}.txt
 
 vi ${tagname}.txt
 
-# git tag -a -F ${tagname}.txt ${tagname}
+# git tag -a -f -F ${tagname}.txt ${tagname}
