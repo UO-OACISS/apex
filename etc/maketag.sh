c0e8b5c90bc4e83da380639df386767fc7c45d40
Kevin Huck <khuck@cs.uoregon.edu>
2019-12-10 14:56:26 -0800

    still fixing release scripts

--- etc/maketag.sh
+++ etc/maketag.sh
@@ -10,9 +10,3 @@ echo "Commit Log:" >> ${tagname}.txt
 
 git log ${oldtag}..HEAD --pretty=format:'<li> <a href="http://github.com/khuck/xpress-apex/commit/%H">view commit &bull;</a> %s</li> ' --reverse > ${tagname}.txt
 # git log ${oldtag}..HEAD --pretty=format:'http://github.com/khuck/xpress-apex/commit/%H %ad %s ' --reverse >> ${tagname}.txt
-
-vi ${tagname}.txt
-
-git tag -a -f -F ${tagname}.txt ${tagname}
-
-git checkout develop
