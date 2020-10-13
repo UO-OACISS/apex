1383d6708b611f7f7b3c8bfc49b3763df35baa35
Kevin Huck <khuck@cs.uoregon.edu>
2020-10-13 09:46:42 -0700

    Enable rapidjson by default

--- cmake/Modules/APEX_SetupPlugins.cmake
+++ cmake/Modules/APEX_SetupPlugins.cmake
@@ -7,29 +7,30 @@ if(APEX_WITH_PLUGINS)
   message(INFO " apex will be built with plugin support.")
   set(LIBS ${LIBS} ${CMAKE_DL_LIBS})
   target_compile_definitions(apex_flags INTERFACE APEX_USE_PLUGINS)
+endif()
 
-  include(GitExternal)
-	git_external(rapidjson
-    https://github.com/miloyip/rapidjson.git
-    master
-    VERBOSE)
+# Regardless, APEX now depends on JSON for event filtering support.
+# So make sure that we have the rapidjson library.
 
-	find_path(
-    RAPIDJSON_INCLUDE_DIR
-    NAMES rapidjson
-    PATHS ${APEX_SOURCE_DIR}/rapidjson/include)
+include(GitExternal)
+  git_external(rapidjson
+  https://github.com/miloyip/rapidjson.git
+  master
+  VERBOSE)
 
-  add_library(rapidjson INTERFACE IMPORTED)
-	if(RAPIDJSON_INCLUDE_DIR)
-    message(INFO " Found rapidjson at ${RAPIDJSON_INCLUDE_DIR}")
-    set_property(TARGET rapidjson PROPERTY
-      INTERFACE_INCLUDE_DIRECTORIES ${RAPIDJSON_INCLUDE_DIR})
+find_path(
+  RAPIDJSON_INCLUDE_DIR
+  NAMES rapidjson
+  PATHS ${APEX_SOURCE_DIR}/rapidjson/include)
 
+add_library(rapidjson INTERFACE IMPORTED)
+if(RAPIDJSON_INCLUDE_DIR)
+  message(INFO " Found rapidjson at ${RAPIDJSON_INCLUDE_DIR}")
+  set_property(TARGET rapidjson PROPERTY
+    INTERFACE_INCLUDE_DIRECTORIES ${RAPIDJSON_INCLUDE_DIR})
   list(APPEND _apex_imported_targets rapidjson)
-
-	else()
-    message(FATAL_ERROR " rapidjson not found. This should have been checked \
-      out automatically. " "Try manually check out \
-      https://github.com/miloyip/rapidjson.git to ${PROJECT_SOURCE_DIR}")
-	endif()
+else()
+  message(FATAL_ERROR " rapidjson not found. This should have been checked \
+    out automatically. " "Try manually check out \
+    https://github.com/miloyip/rapidjson.git to ${PROJECT_SOURCE_DIR}")
 endif()
