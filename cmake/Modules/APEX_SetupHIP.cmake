893fc39f75f8388a3ea130d04356ea6398aa3d9d
Kevin Huck <khuck@cs.uoregon.edu>
2021-09-29 12:43:44 -0700

    Forgot RSMI for HIP support in HPX

--- cmake/Modules/APEX_SetupHIP.cmake
+++ cmake/Modules/APEX_SetupHIP.cmake
@@ -6,10 +6,12 @@
 if(APEX_WITH_HIP)
   find_package(ROCTRACER REQUIRED QUIET COMPONENTS ROCTRACER)
   find_package(ROCTX REQUIRED QUIET COMPONENTS ROCTX)
+  find_package(RSMI REQUIRED)
 
   # Add an imported target
   add_library(roctracer INTERFACE IMPORTED)
   add_library(roctx INTERFACE IMPORTED)
+  add_library(rsmi INTERFACE IMPORTED)
   hpx_info("apex" "Building APEX with HIP/ROCTRACER support.")
   set_property(TARGET roctracer PROPERTY
     INTERFACE_INCLUDE_DIRECTORIES ${ROCTRACER_INCLUDE_DIRS})
@@ -19,6 +21,10 @@ if(APEX_WITH_HIP)
     INTERFACE_INCLUDE_DIRECTORIES ${ROCTX_INCLUDE_DIRS})
   set_property(TARGET roctx PROPERTY
     INTERFACE_LINK_LIBRARIES ${ROCTX_LIBRARIES})
+  set_property(TARGET rsmi PROPERTY
+    INTERFACE_INCLUDE_DIRECTORIES ${RSMI_INCLUDE_DIRS})
+  set_property(TARGET rsmi PROPERTY
+    INTERFACE_LINK_LIBRARIES ${RSMI_LIBRARIES})
 
   # Add the right definitions to the apex_flags target
   target_compile_definitions(apex_flags INTERFACE APEX_WITH_HIP)
@@ -26,6 +32,7 @@ if(APEX_WITH_HIP)
 
   list(APPEND _apex_imported_targets roctracer)
   list(APPEND _apex_imported_targets roctx)
+  list(APPEND _apex_imported_targets rsmi)
 else()
   add_custom_target(project_hip)
 endif()
