5582d0ab0996bee3496ae6b2b04c102db8b7a11a
Kevin Huck <khuck@cs.uoregon.edu>
2022-04-14 12:57:34 -0700

    The HPX configuration was missing ROCPROFILER

--- cmake/Modules/APEX_SetupHIP.cmake
+++ cmake/Modules/APEX_SetupHIP.cmake
@@ -5,18 +5,28 @@
 
 if(APEX_WITH_HIP)
   find_package(ROCTRACER REQUIRED QUIET COMPONENTS ROCTRACER)
+  find_package(ROCPROFILER REQUIRED QUIET COMPONENTS ROCPROFILER)
   find_package(ROCTX REQUIRED QUIET COMPONENTS ROCTX)
   find_package(RSMI REQUIRED)
 
   # Add an imported target
   add_library(roctracer INTERFACE IMPORTED)
-  add_library(roctx INTERFACE IMPORTED)
-  add_library(rsmi INTERFACE IMPORTED)
   hpx_info("apex" "Building APEX with HIP/ROCTRACER support.")
+  add_library(rocprofiler INTERFACE IMPORTED)
+  hpx_info("apex" "Building APEX with HIP/ROCPROFILER support.")
+  add_library(roctx INTERFACE IMPORTED)
+  hpx_info("apex" "Building APEX with HIP/ROCTX support.")
+  add_library(rsmi INTERFACE IMPORTED)
+  hpx_info("apex" "Building APEX with HIP/ROC-SMI support.")
+
   set_property(TARGET roctracer PROPERTY
     INTERFACE_INCLUDE_DIRECTORIES ${ROCTRACER_INCLUDE_DIRS})
   set_property(TARGET roctracer PROPERTY
     INTERFACE_LINK_LIBRARIES ${ROCTRACER_LIBRARIES})
+  set_property(TARGET rocprofiler PROPERTY
+    INTERFACE_INCLUDE_DIRECTORIES ${ROCPROFILER_INCLUDE_DIRS})
+  set_property(TARGET rocprofiler PROPERTY
+    INTERFACE_LINK_LIBRARIES ${ROCPROFILER_LIBRARIES})
   set_property(TARGET roctx PROPERTY
     INTERFACE_INCLUDE_DIRECTORIES ${ROCTX_INCLUDE_DIRS})
   set_property(TARGET roctx PROPERTY
@@ -31,6 +41,7 @@ if(APEX_WITH_HIP)
   target_compile_definitions(apex_flags INTERFACE __HIP_PLATFORM_HCC__)
 
   list(APPEND _apex_imported_targets roctracer)
+  list(APPEND _apex_imported_targets rocprofiler)
   list(APPEND _apex_imported_targets roctx)
   list(APPEND _apex_imported_targets rsmi)
 else()
