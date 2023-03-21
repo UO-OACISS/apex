45a2823393fe255eb5b2be009462eab9f98945df
Kevin Huck <khuck@cs.uoregon.edu>
2023-03-20 18:44:32 -0700

    Adding sleep to version tests so they don't exit before APEX has even initialized.

--- src/unit_tests/C++/apex_version.cpp
+++ src/unit_tests/C++/apex_version.cpp
@@ -6,6 +6,7 @@ int main (int argc, char** argv) {
   APEX_UNUSED(argv);
   apex::init("apex::version unit test", 0, 1);
   std::cout << apex::version() << std::endl;
+  sleep(1);
   apex::finalize();
   apex::cleanup();
   return 0;
