7b988329d39384825f47db8fba3856e55b75bfdb
Kevin Huck <khuck@cs.uoregon.edu>
2024-02-22 09:43:39 -0800

    Updating version number for 2.6.5 patch release

--- src/apex/CMakeLists_hpx.cmake
+++ src/apex/CMakeLists_hpx.cmake
@@ -19,7 +19,7 @@ hpx_info("apex" "Will build APEX")
 
 set (APEX_VERSION_MAJOR 2)
 set (APEX_VERSION_MINOR 6)
-set (APEX_VERSION_PATCH 4)
+set (APEX_VERSION_PATCH 5)
 
 if (NOT APEX_ROOT)
   if (EXISTS ${HPX_SOURCE_DIR}/apex)
