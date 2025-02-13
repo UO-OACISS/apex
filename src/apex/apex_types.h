9939556bc81cc753c5834f777c23581b65e4ff4c
Kevin Huck <khuck@cs.uoregon.edu>
2025-02-13 09:00:42 -0800

    Fixing OpenCL support, don't hard-code the library name. It is vendor dependent. Also, if/when it can't be found by CMake, allow for the user to specify it with -DOpenCL_LIBRARY=<some value>

--- src/apex/apex_types.h
+++ src/apex/apex_types.h
@@ -397,7 +397,8 @@ inline unsigned int sc_nprocessors_onln(void)
     macro (APEX_KOKKOS_TUNING_CACHE, kokkos_tuning_cache, char*, "", "Filename contining Kokkos autotuned results, tuned offline.") \
     macro (APEX_KOKKOS_TUNING_POLICY, kokkos_tuning_policy, char*, "simulated_annealing", "Kokkos autotuning policy: random, exhaustive, simulated_annealing, nelder_mead, automatic.") \
     macro (APEX_ROCPROF_METRICS, rocprof_metrics, char*, "", "List of metrics to periodically sample with the Rocprofiler library (see /opt/rocm/rocprofiler/lib/metrics.xml).") \
-    macro (APEX_NVTX_LIBRARY, nvtx_library, char*, "libnvToolsExt.so", "With NVTX listener, specify the location of libnvToolsExt.so.")
+    macro (APEX_NVTX_LIBRARY, nvtx_library, char*, "libnvToolsExt.so", "With NVTX listener, specify the location of libnvToolsExt.so.") \
+    macro (APEX_OPENCL_LIBRARY, opencl_library, char*, "libOpenCL.so", "Specify the name and/or path to the OpenCL library.")
     // macro (APEX_ROCPROF_METRICS, rocprof_metrics, char*, "MemUnitBusy,MemUnitStalled,VALUUtilization,VALUBusy,SALUBusy,L2CacheHit,WriteUnitStalled,ALUStalledByLDS,LDSBankConflict", "")
 
 #if defined(_WIN32) || defined(_WIN64)
